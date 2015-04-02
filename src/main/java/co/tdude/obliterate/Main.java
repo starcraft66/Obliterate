440d3cb73d05704e37440572d0fb0ed2dbbaa4ab
Tristan Gosselin-Hane <starcraft66@gmail.com>
2015-04-02 13:21:06 -0400

    Added cooldown with config file

--- src/main/java/co/tdude/obliterate/Main.java
+++ src/main/java/co/tdude/obliterate/Main.java
@@ -1,25 +1,44 @@
 package co.tdude.obliterate;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Tristan on 2015-04-02.
  */
 public class Main extends JavaPlugin {
 
+    Map<UUID, Long> users;
+    String cooldownmessage;
+    Long cooldown;
+
     @Override
     public void onEnable() {
         getLogger().info("Obliterate by Tristan enabled!");
+        users = new ConcurrentHashMap<UUID, Long>();
+        cooldown = getConfig().getLong("kill-cooldown");
+        cooldownmessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("cooldown-message"));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("kill")) {
             if (sender instanceof Player) {
-                ((Player) sender).setHealth(0);
+                if (users.get(((Player) sender).getUniqueId()) == null) {
+                    ((Player) sender).setHealth(0);
+                    users.put(((Player) sender).getUniqueId(), System.currentTimeMillis());
+                } else if (users.get(((Player) sender).getUniqueId()) != null && users.get(((Player) sender).getUniqueId()) + (cooldown * 1000) <= System.currentTimeMillis()) {
+                    ((Player) sender).setHealth(0);
+                } else {
+                    sender.sendMessage(cooldownmessage);
+                }
                 return true;
             } else {
                 sender.sendMessage("You are not a player.");
